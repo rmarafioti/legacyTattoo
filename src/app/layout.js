f3d90b9ecf14238006fdc08b8ac7332af571d348
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2025-01-23 20:38:50 -0600

    choose font styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Assistant } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const assistant = Assistant({
+  variable: "--assistant",
+  weight: ["400", "600", "700"],
   subsets: ["latin"],
 });
 
@@ -19,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={`${assistant.variable}`}>{children}</body>
     </html>
   );
 }
