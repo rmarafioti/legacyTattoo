ce57ef6e22c0deb1fb805660e337f30a219d130f
Richard Marafioti <richmarafioti@gmail.com>
2025-01-31 10:30:26 -0600

    deleted commented out code

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import ContactForm from "./components/ContactForm";
 import styles from "./page.module.css";
